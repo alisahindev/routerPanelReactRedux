29e43fa4901adc264f0b53bb1b945cffc3f6c37c
alisahindev <alisahindev@gmail.com>
2020-08-30 00:33:43 +0300

    .

--- src/pages/communities/communityDetail.jsx
+++ src/pages/communities/communityDetail.jsx
@@ -5,7 +5,7 @@ import {
   communityDetailClear,
   updateCommunityRequest,
 } from "../../redux/communities/action";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Col, Row, Divider } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import moment from "moment";
 import Avatar from "antd/lib/avatar/avatar";
@@ -34,53 +34,65 @@ class CommunityDetail extends Component {
   };
 
   render() {
-    const formItemLayout = {
+    const layout = {
       labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 },
+        span: 4,
       },
       wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
+        span: 16,
       },
     };
-
     const { detailCommunity } = this.props;
     return (
-      <Form onFinish={this.onFinish} ref={this.formRef} {...formItemLayout}>
-        <Form.Item key="name" name="name" label="Topluluk Adı">
-          <Input placeholder="Topluluk Adı" />
-        </Form.Item>
-        <Form.Item key="slug" name="slug" label="Topluluk Slug">
-          <Input placeholder="Topluluk Slug" />
-        </Form.Item>
-        <Form.Item key="description" name="description" label="Açıklama">
-          <Input.TextArea placeholder="Topluluk açıklaması" />
-        </Form.Item>
-
-        <Form.Item
-          key="createdDate"
-          name="createdDate"
-          label="Oluşturulma Tarihi"
-        >
-          <Input placeholder="Oluşturulma tarihi" />
-        </Form.Item>
-
-        <Form.Item key="logoPath" label="Logo">
-          <Avatar
-            size={250}
-            src={detailCommunity && detailCommunity.logoPath}
-            icon={<UserOutlined />}
-          />
-          <Button
-            style={{ marginLeft: "3rem" }}
-            type="primary"
-            htmlType="submit"
+      <>
+        <Divider orientation="left">
+          Topluluk Bilgisi - {detailCommunity.name}
+        </Divider>
+        <Row>
+          <Col
+            span={24}
+            xs={{ order: 4 }}
+            sm={{ order: 3 }}
+            md={{ order: 1 }}
+            lg={{ order: 2 }}
           >
-            Güncelle
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form onFinish={this.onFinish} ref={this.formRef} {...layout}>
+              <Form.Item key="name" name="name" label="Topluluk Adı">
+                <Input placeholder="Topluluk Adı" />
+              </Form.Item>
+              <Form.Item key="slug" name="slug" label="Topluluk Slug">
+                <Input placeholder="Topluluk Slug" />
+              </Form.Item>
+              <Form.Item key="description" name="description" label="Açıklama">
+                <Input.TextArea placeholder="Topluluk açıklaması" />
+              </Form.Item>
+
+              <Form.Item
+                key="createdDate"
+                name="createdDate"
+                label="Oluşturulma Tarihi"
+              >
+                <Input placeholder="Oluşturulma tarihi" />
+              </Form.Item>
+
+              <Form.Item key="logoPath" label="Logo">
+                <Avatar
+                  size={250}
+                  src={detailCommunity && detailCommunity.logoPath}
+                  icon={<UserOutlined />}
+                />
+                <Button
+                  style={{ marginLeft: "3rem" }}
+                  type="primary"
+                  htmlType="submit"
+                >
+                  Güncelle
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
