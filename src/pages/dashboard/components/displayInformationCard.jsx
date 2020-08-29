0ffe271213449e508a927039ea0587dd6b9d7c17
alisahindev <alisahindev@gmail.com>
2020-08-29 21:04:14 +0300

    .

--- src/pages/dashboard/components/displayInformationCard.jsx
+++ src/pages/dashboard/components/displayInformationCard.jsx
@@ -7,41 +7,53 @@ const DisplayInformationCard = ({ users, communities }) => {
     <div className="site-statistic-demo-card">
       <Row gutter={16}>
         <Col span={6}>
-          <Card className="dashboard-card">
+          <Card
+            style={{ backgroundColor: "#e91e63" }}
+            className="dashboard-card"
+          >
             <Statistic
               title="Toplam Kullanıcı"
               value={users && users.length}
-              valueStyle={{ color: "#3f8600" }}
+              valueStyle={{ color: "#fff" }}
               prefix={<SmileFilled />}
             />
           </Card>
         </Col>
         <Col span={6}>
-          <Card className="dashboard-card">
+          <Card
+            style={{ backgroundColor: "#00bcd4" }}
+            className="dashboard-card"
+          >
             <Statistic
               title="Toplam Topluluk"
               value={communities && communities.length}
-              valueStyle={{ color: "#cf1322" }}
+              valueStyle={{ color: "#fff" }}
               prefix={<ProjectFilled />}
             />
           </Card>
         </Col>
         <Col span={6}>
-          <Card className="dashboard-card">
+          <Card
+            style={{ backgroundColor: "#8bc34a" }}
+            className="dashboard-card"
+          >
             <Statistic
-              title="Toplam Topluluk"
+              title="Kategori Sayısı"
               value={communities && communities.length}
-              valueStyle={{ color: "#cf1322" }}
+              valueStyle={{ color: "#fff" }}
               prefix={<ProjectFilled />}
             />
           </Card>
         </Col>
         <Col span={6}>
-          <Card className="dashboard-card">
+          <Card
+            style={{ backgroundColor: "#ff9800" }}
+            className="dashboard-card"
+          >
             <Statistic
-              title="Toplam Topluluk"
+              title="Toplam Gönderiler"
               value={communities && communities.length}
-              valueStyle={{ color: "#cf1322" }}
+              valueStyle={{ color: "#fff" }}
               prefix={<ProjectFilled />}
             />
           </Card>
